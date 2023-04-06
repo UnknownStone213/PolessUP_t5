6754fc5bfc6a296640c9027215090dcbcc408cbf
Vlad <vlad.khvesyukovich@gmail.com>
2023-04-06 12:46:44 +0300

    t5 is done

---
+++ code/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of array:");
+            int[] nums = new int[Convert.ToInt32(Console.ReadLine())];
+            Console.WriteLine("Fill the array:");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("nums[{0}] = ", i);
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Array has been filled.");
+
+            int index = 0;
+            int max = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (max < nums[i])
+                {
+                    max = nums[i];
+                    index = i;
+                }
+            }
+            Console.WriteLine("Output (position of max element) = " + index);
+
+
+            Console.ReadLine();
+        }
+    }
+}
